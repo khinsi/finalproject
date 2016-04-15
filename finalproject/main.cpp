54c161526966208a8bd2bac8144fbac9e4eee952
Khin Yatana <khinyatana@khins-imac.local>
2016-04-15 09:27:39 -0500

    Initial Commit

---
+++ finalproject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  finalproject
+//
+//  Created by Khin Yatana on 4/15/16.
+//  Copyright © 2016 Khin Yatana. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
